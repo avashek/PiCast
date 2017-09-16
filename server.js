97e290e256fbf744fc9130199c24481b0aa78474
avashek <keshavar007@gmail.com>
2017-09-16 11:14:48 +0530

    ..

--- server.js
+++ server.js
@@ -8,27 +8,23 @@ exec = require('child_process').exec,
 ip = require('ip');
 
 app.set('port', process.env.TEST_PORT || 8080);
-app.use(express.static(path.join(__dirname, '/src')));
+app.use(express.static(path.join(__dirname,'/src')));
 const pipe = false;
 
-app.get('/', (req, res) => {
-res.sendFile(__dirname + '/src/index.html');
+app.get('/',(req,res)=>{
+res.sendFile(__dirname+'/src/index.html');
 })
-var video = {};
-var pauseState = 'play';
-io.sockets.on('connection', (socket) => {
-socket.on('video', (id,json) => {
-  video = json;
-  socket.emit('currVideo',json);
 
+io.sockets.on('connection', (socket) => {  
+socket.on('video', (id) => {
   url = 'https://www.youtube.com/watch?v=' + id;
   const child = new spawn('youtube-dl', ['-g', '-f', '22/18', url]);
   const omx_url = '';
 
-  if (pipe) {
-    pipe = false;
-    new exec('echo -n q > /tmp/pipe');
-    new exec('rm /tmp/pipe');
+  if(pipe){
+pipe = false; 
+new exec('echo -n q > /tmp/pipe');
+new exec('rm /tmp/pipe');
   }
 
   child.stdout.on('data', (data) => {
@@ -38,33 +34,27 @@ socket.on('video', (id,json) => {
 
   child.stdout.on('end', () => {
     new exec('mkfifo /tmp/pipe');
-    new exec('omxplayer -o local "' + omxurl + '" < /tmp/pipe ');
+    new exec('omxplayer -o local "'+omxurl+'" < /tmp/pipe ');
     new exec('echo . > /tmp/pipe');
     pipe = true;
   });
-  console.log('Casting ' + id);
+  console.log('Casting '+id);
   console.log('\nStarting video...');
 })
 socket.on('key', (key) => {
   key = key.toString().trim();
   console.log('key', key);
-  new exec('echo -n ' + key + ' > /tmp/pipe');
-
-  if(key === 'p') {
-    pauseState = pauseState === 'play'? 'pause' : 'play';
-    socket.emit('pause',pauseState);
-    console.log(pauseState);
-  }
-  if (key === 'q') {
-    pauseState = 'play';
+  new exec('echo -n '+key+' > /tmp/pipe');
+  
+  if(key === 'q'){
+    //new exec('rm /tmp/pipe');
     pipe = false;
   }
 })
-
 })
 
 server.listen(app.get('port'), () => {
 ip.address();
-console.log('Picast running on http://' + ip.address() + ':' + app.get('port'));
+console.log('Picast running on http://'+ip.address() +':'+ app.get('port'));
 console.log('');
 })
